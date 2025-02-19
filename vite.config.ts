ed646612159826ce8c9c6ce1277d964ef4f1d701
jonoz <jono.z.w4@gmail.com>
2025-02-19 14:43:35 -0500

    more fixes yayayay

--- vite.config.ts
+++ vite.config.ts
@@ -15,10 +15,22 @@ export default defineConfig({
         target: 'https://cloud.appwrite.io',
         changeOrigin: true,
         secure: true,
+        ws: true,
         headers: {
-          'Origin': 'https://www.livity.app'
+          'Origin': 'https://cloud.appwrite.io'
+        },
+        configure: (proxy) => {
+          proxy.on('error', (err) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (_, req) => {
+            console.log('Sending Request to the Target:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req) => {
+            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          });
         }
       }
     }
   }
-})
+});
